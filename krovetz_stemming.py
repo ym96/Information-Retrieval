849605540a8015b53371ec7de6803d83ce754535
Yash Mishra <47400738+ym96@users.noreply.github.com>
2020-12-05 15:44:47 -0500

    Update krovetz_stemming.py

--- krovetz_stemming.py
+++ krovetz_stemming.py
@@ -23,6 +23,70 @@ with gzip.open("C:\ClueWeb09\2008-01-01", "rb") as f1:
     trecCW = norm.pdf(x, 0, 2)
     q = norm.pdf(x, 1, 1)
 
+with gzip.open("C:\ClueWeb09\2008-02-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW1 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-02-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW2 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-03-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW3 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-04-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW4 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+
+with gzip.open("C:\ClueWeb09\2008-05-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW5 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-06-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW5 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+
+with gzip.open("C:\ClueWeb09\2008-07-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW6 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-08-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW7= norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+
+with gzip.open("C:\ClueWeb09\2008-09-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW8= norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+with gzip.open("C:\ClueWeb09\2008-10-01", "rb") as f1:
+    file_content = f1.read()
+    x = f1.readline()
+    trecCW9 = norm.pdf(x, 0, 2)
+    q = norm.pdf(x, 1, 1)
+
+
 # 
 # clue_web_01_08_df = pd.read_csv('C:\ClueWeb09\2008-01-01\en0000\01.warc.gz', compression='gzip',
 #                                  header=1, sep='\t', quotechar='"')
@@ -34,8 +98,21 @@ for record in f2:
     print (record['Trec-'], record['Content-Length'])
 
 
-f3 - warc.open("C:\Information Retrieval\ClueWeb09\2008-02-01\en0000\01.warc.gz")
+f3 = warc.open("C:\Information Retrieval\ClueWeb09\2008-02-01\en0000\01.warc.gz")
 for record in f3:
+    print (record['Trec-'], record['Content-Length'])
+
+f4 = warc.open("C:\Information Retrieval\ClueWeb09\2008-03-01\en0000\01.warc.gz")
+for record in f4:
+    print (record['Trec-'], record['Content-Length'])
+
+f5 = warc.open("C:\Information Retrieval\ClueWeb09\2008-04-01\en0000\01.warc.gz")
+for record in f4:
+    print (record['Trec-'], record['Content-Length'])
+
+
+f6 = warc.open("C:\Information Retrieval\ClueWeb09\2008-05-01\en0000\01.warc.gz")
+for record in f6:
     print (record['Trec-'], record['Content-Length'])
 
 # Access document-query-mapppings for the ClueWeb09 web 
@@ -58,5 +135,29 @@ q = norm.pdf(x, 2, 3)
 plt.title('KL(P||Q) = %1.3f' % KL_Divergence(q, p))
 plt.plot(x, p)
 plt.plot(x, q, c='blue')
+
+
+plt.title("Temporal document changes")
+plt.plot(trecCW1, p)
+plt.plot(trecCW2, p, c="red") 
+
+
+plt.title("Temporal document changes")
+plt.plot(trecCW1, p)
+plt.plot(trecCW3, p, c="orange") 
+
+
+plt.title("Temporal document changes")
+plt.plot(trecCW1, p)
+plt.plot(trecCW4, p, c="yellow") 
+
+plt.title("Temporal document changes")
+plt.plot(trecCW1, p)
+plt.plot(trecCW5, p, c="brown") 
+
+
+plt.title("Temporal document changes")
+plt.plot(trecCW1, p)
+plt.plot(trecCW6, p, c="green") 
 
 
